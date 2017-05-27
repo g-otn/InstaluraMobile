8a5fedbcab10bec0bf94de1500c4ff23f968e984
Rafael Rollo <rollo@Rafaels-MacBook-Pro.local>
2017-05-27 18:39:16 -0300

    Renderizando imagens
    Conhecendo React Native

---
+++ src/app.js
@@ -0,0 +1,49 @@
+/**
+ * InstaluraMobile
+ * https://github.com/rafael-rollo/InstaluraMobile
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  Image,
+  Dimensions,
+  Platform
+} from 'react-native';
+
+const width = Dimensions.get('window').width;
+const paddingTop = Platform.OS === 'ios' ? 20 : 0;
+
+class InstaluraMobile extends Component {
+
+  render() {
+    return (
+      <FlatList
+          style={{paddingTop}}
+          data={[{id: 1, usuario: 'rafael'}, {id: 2, usuario: 'alberto'}]}
+          keyExtractor={ item => item.id }
+          renderItem={ ({item}) => (
+            <View>
+              <View style={{height: 40}}>
+                <Text>{item.usuario}</Text>
+              </View>
+              <Image style={{width: width, height: width}}
+                  source={require('./resources/img/instagram1.jpg')} />
+            </View>
+          )}
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default () => {
+  AppRegistry.registerComponent('InstaluraMobile', () => InstaluraMobile);
+}
