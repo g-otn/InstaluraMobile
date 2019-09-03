1b9eddee5160ef6b27741ca1856947bcf6a3d211
g-otn <g-otn@hotmail.com>
2019-09-03 11:00:49 -0300

    Atualizar layout e refatorar estilos

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import {
   View,
   Text,
   Dimensions,
-  FlatList
+  FlatList,
+  StyleSheet
 } from 'react-native'
 
 const width = Dimensions.get('screen').width
@@ -14,15 +15,20 @@ const App = () => {
 
   return (
     <FlatList
-      style={{marginTop: 20}}
+      style={styles.container}
       keyExtractor={item => item.id.toString()} // https://stackoverflow.com/a/49577737
       data={fotos}
       renderItem={({item}) => {
         return (
           <View>
-            <Text>{item.usuario}</Text>
+            <View style={styles.cabecalho}>
+              <Image source={require('./assets/img/alura.jpg')}
+                style={styles.fotoDePerfil}
+              />
+              <Text>{item.usuario}</Text>
+            </View>
             <Image source={require('./assets/img/alura.jpg')}
-              style={{ width: width, height: width }}
+              style={styles.foto}
             />
           </View>
         )
@@ -30,5 +36,26 @@ const App = () => {
     />
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 20
+  },
+  cabecalho: {
+    margin: 10, 
+    flexDirection: 'row', 
+    alignItems: 'center'
+  },
+  fotoDePerfil: {
+    marginRight: 10, 
+    borderRadius: 25, 
+    width: 50, 
+    height: 50 
+  },
+  foto: { 
+    width: width, 
+    height: width 
+  }
+})
 
 export default App
